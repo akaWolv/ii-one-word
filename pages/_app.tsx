72f39cb00fff7c4f76eb461c8c59f86ac46d67da
Paweł Nejczew <pawel.nejczew@wakacje.pl>
2022-08-12 00:28:34 +0200

    * fix: display * fix: repeats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function MyApp (props: AppProps) {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>One-word</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
